d16557185f3b8707681030bbd57922ecee6449bd
Vukile <vukile@healthsystems.co.za>
2021-04-19 16:46:42 +0200

    First commit on our csv read project

---
+++ MyfirstMavenProjectIT/src/test/java/readFromCSVvukile/ReadFromCSV.java
@@ -0,0 +1,117 @@
+package readFromCSVvukile;
+
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import frameworkclasses.SeleniumFunctions;
+
+
+public class ReadFromCSV {
+	
+	SeleniumFunctions sfSelenium = new SeleniumFunctions();
+	
+	// driver variable
+	WebDriver driver;
+	
+	public String getProperties(String pPropertyKey) {
+		// Properties setup
+				Properties p = new Properties();
+				InputStream is = null;
+				try {
+					is = new FileInputStream("dataConfig.properties");
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					p.load(is);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		return p.getProperty(pPropertyKey);
+	}
+	
+
+	
+	// Navigate to demo.guru99.com
+	public void navigateToURL(String pURL) {
+		this.driver.get(pURL);
+		sfSelenium.maximiseBrowserWindow();	
+	}
+	
+	public void clickLogin() {
+		
+		this.driver.findElement(By.xpath("//input[@value='LOGIN']")).click();
+	}
+	
+	public void captureDetails(String pUsername, String pPassword) {
+		
+		sfSelenium.populateInputField(By.name("uid"), pUsername);
+		sfSelenium.populateInputField(By.name("password"), pPassword);
+		
+		clickLogin();
+		
+		
+	}
+	
+	public void runTest() throws IOException {
+		this.driver = sfSelenium.getDriver();
+		sfSelenium.startReport("Read From CSV Report", "CSV Report");
+		sfSelenium.createTest("Start Test");
+		// Set URL
+		String pURL = "http://demo.guru99.com/V1/index.php";
+		
+		navigateToURL(pURL);
+		readCSV();
+		
+		
+	}
+	
+	public void readCSV() throws IOException {
+		
+		Properties p = new Properties();
+		InputStream is = null;
+		try {
+			is = new FileInputStream("dataConfig.properties");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			p.load(is);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(p.getProperty("csvLoc"));
+		
+		BufferedReader br = new BufferedReader(new FileReader(p.getProperty("csvLoc"))); 
+		String line;
+		while ((line = br.readLine()) != null) { 
+		    // use xx as separator 
+		    // Task change the delimiter to a ,
+			// assign a variable to each field value
+			System.out.println(line);
+			
+			String[] cols = line.split(";"); 
+		    String pUsername = cols[0];
+		    String pPassword = cols[1];
+		    captureDetails(pUsername, pPassword);
+		    
+		    //System.out.println(username + " " + password );
+	}
+		
+}
+
+}
---
+++ MyfirstMavenProjectIT/src/test/java/readFromCSVvukile/ReadFromCSVRun.java
@@ -0,0 +1,15 @@
+package readFromCSVvukile;
+
+import java.io.IOException;
+
+public class ReadFromCSVRun {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+     
+	  ReadFromCSV objRead = new ReadFromCSV();
+	  
+	  objRead.runTest();
+	}
+
+}
