22d20e285dfb509c7043056f219f56ea9a9e2f7b
Mvuyo <Mvuyo@HSTCPT-Mvuyo.HST.local>
2021-05-03 15:09:53 +0200

    interface classes (Mvuyo Mdekazi)

---
+++ MyfirstMavenProjectIT/src/test/java/testngListeners/TestNGListenerDemo.java
@@ -0,0 +1,27 @@
+package testngListeners;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(testngListeners.TestNGlisteners.class)
+
+public class TestNGListenerDemo {
+	
+	@Test
+	public void test1() {
+		System.out.println("I am in test 1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("I am in test 2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("I am in test 3");
+	}
+
+
+}
+
