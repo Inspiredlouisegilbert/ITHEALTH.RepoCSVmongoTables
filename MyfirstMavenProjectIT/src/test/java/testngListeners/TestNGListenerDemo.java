312329fcde9b50e7bffa2e5097fce96a6363349e
Junaid <Junaid@hstcpt-intern.HST.local>
2021-05-03 15:21:45 +0200

    Add a Failed and a skip(junaid)

--- MyfirstMavenProjectIT/src/test/java/testngListeners/TestNGListenerDemo.java
+++ MyfirstMavenProjectIT/src/test/java/testngListeners/TestNGListenerDemo.java
@@ -1,5 +1,7 @@
 package testngListeners;
 
+import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -15,11 +17,13 @@ public class TestNGListenerDemo {
 	@Test
 	public void test2() {
 		System.out.println("I am in test 2");
+		Assert.assertTrue(false);
 	}
 	
 	@Test
 	public void test3() {
 		System.out.println("I am in test 3");
+		throw new SkipException("this is a skipped test" );
 	}
 
 
