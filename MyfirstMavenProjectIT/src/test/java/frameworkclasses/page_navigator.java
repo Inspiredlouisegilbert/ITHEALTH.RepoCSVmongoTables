4f7a3970ec542bc684781c20aa96353b06edfbe6
Junaid <Junaid@hstcpt-intern.HST.local>
2021-06-07 16:49:14 +0200

    page navigator

---
+++ MyfirstMavenProjectIT/src/test/java/frameworkclasses/page_navigator.java
@@ -0,0 +1,26 @@
+package frameworkclasses;
+
+import frameworkclasses.BasePage;
+
+public class page_navigator extends BasePage {
+   String Url = "";
+	public void setuptest() {
+		
+		driver.navigate().to(Url);
+	}
+	
+	public void getFromprop(String parameterName) {
+	
+		Url= getDataConfigProperties(parameterName);
+		setuptest();
+	}
+	
+	public void setCurrentUrl() {
+		Url = driver.getCurrentUrl();
+	}
+	
+	public void goToPrevTestPage() {
+		
+		driver.get(Url);
+	}
+}
