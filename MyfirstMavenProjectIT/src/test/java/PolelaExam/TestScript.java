921cf47dadb156750a1801cccef37bdd67ccd4ab
Polela <Polela@HSTCPT-MOGAMATA.HST.local>
2021-07-14 14:39:52 +0200

    newly changed code

--- MyfirstMavenProjectIT/src/test/java/PolelaExam/TestScript.java
+++ MyfirstMavenProjectIT/src/test/java/PolelaExam/TestScript.java
@@ -5,25 +5,31 @@ import org.testng.annotations.Test;
 
 public class TestScript {
 
+
 	HomePage home = new HomePage();
 	BrandPage brand = new BrandPage();
 	ProductPage prod = new ProductPage();
 	
-	@Test
-	public void EnterSearch () {
-		
+	@Test (priority=0)
+	public void StartTest() {
 	home.clickBargain();
 	brand.selectBrand("Dell");
 	brand.waitFunction("Dell");
+	}
+
+	@Test(priority = 1)
+	public void EnterSearch () {
+
 	brand.selectProduct();
 	prod.rateProduct();
 	String Actual = prod.getErrorMessage();
-	String Expected = "";
-	
+	String Expected = "You cannot post a review because you are either not logged in or you have not purchased this product";
 	Assert.assertEquals(Actual, Expected);
-	
+
+	}
+	@Test (priority =2)
+	public void Results() {
 	prod.clickClose();
-	
 	prod.clickAddToCart();
 	}
 	
