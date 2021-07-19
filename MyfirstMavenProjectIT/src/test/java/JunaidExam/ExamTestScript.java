3d238d18aab6e5d81cdf00d45dcf9e7f43027458
Junaid <Junaid@hstcpt-intern.HST.local>
2021-07-19 14:49:16 +0200

    Reporting and BDD

--- MyfirstMavenProjectIT/src/test/java/JunaidExam/ExamTestScript.java
+++ MyfirstMavenProjectIT/src/test/java/JunaidExam/ExamTestScript.java
@@ -3,6 +3,7 @@ package JunaidExam;
 import java.io.IOException;
 
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.SkipException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -20,25 +21,53 @@ public class ExamTestScript {
 	public void RateItTest(String pBrand) {
 		
 		if (pBrand == null) {
+			Reporter.log("value given is null");
 			throw new SkipException("value given is null");
-		}else {
+		}
+		else {
 		//Click on Bargin Bin Link
 		home.clickBargain();
+		Reporter.log("Clicked on Bargin Bin Link");
+		
 		//Select a Brand
 		bargin.clickBrand(pBrand);
 		bargin.waitToLoad(pBrand);
+		Reporter.log("Clicked Brand filter");
+
 		//Click the First Item
 		bargin.clickProduct(1);
+		Reporter.log("Clicked on first product");
+
 		//Click the Rate It button
 		item.clickRateProduct();
-		//Assert Error message to equal You cannot post a review because you are either not logged in or you have not purchased this product 
+		Reporter.log("Clicked on rate it button");
+		
+		//Assert message to equal You cannot post a review because you are either not logged in or you have not purchased this product 
 		String Actual = item.getErrorMessage();
 		String Expected = "You cannot post a review because you are either not logged in or you have not purchased this product";
+		if (Actual == Expected) {
+			Reporter.log("Test Passed");
+			Reporter.log("expected ------------------"+Expected);
+	        Reporter.log("actual --------------------"+Actual);
+		}
+		else {
+			Reporter.log("Test Passed");
+			Reporter.log("expected ------------------"+Expected);
+	        Reporter.log("actual --------------------"+Actual);
+		}
 		Assert.assertEquals(Actual, Expected);
 		
+//		Handle message
 		item.clickClose();
+		
+//		CLick add to cart
 		item.clickAddToCart();
+		Reporter.log("Clicked Add to Cart");
+
+//		Click Proceed to shop
 		item.proceedToShop();
+		Reporter.log("Clicked Proceed to Shop");
+
 		}
 	}
 	
